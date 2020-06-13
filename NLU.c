73f72ccf77f09aab3ca24c068776b34afcb51590
shreyasriram4 <65442252+shreyasriram4@users.noreply.github.com>
2020-06-14 01:38:03 +0800

    Add files via upload

---
+++ NLU.c
@@ -0,0 +1,22 @@
+curl -X POST -u "apikey:CHRzO3OEFwiIeoFY8GsWy-_Y_Y39c1gcQmirziT8NfMZ" \
+--header "Content-Type: application/json" \
+--data '{
+  "text": "I stay in Block 123 Yishun Ring Road",
+  "features": {
+
+    "sentiment": {
+      "targets": [
+        "Block 123 Yishun Ring Road",
+        "Block 123 Bedok Road",
+        "Block 122 Yishun Ring Road"
+      ]
+    },
+    "keywords": {
+      "emotion": false
+    }
+  }
+}' \
+"https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/71dd1904-c98f-4be6-bbdf-762ea3220067/v1/analyze?version=2019-07-12"
+
+/*under targets, strings of each possible address in database of singapore address will be included.
+/*through this, if the keyword of that address is identified in "text", this keyword will be raised as the location of caller.
